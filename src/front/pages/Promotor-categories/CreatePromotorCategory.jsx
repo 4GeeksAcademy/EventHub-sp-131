8c79489a0d497c237e25909a8ab6876cb2d7e643
CarolinaNFeria <rogua20.12@gmail.com>
2026-04-23 10:07:32 +0000

    fixing url

--- src/front/pages/Promotor-categories/CreatePromotorCategory.jsx
+++ src/front/pages/Promotor-categories/CreatePromotorCategory.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { PromotorCategoryForm } from "../../components/PromotorCategoryForm";
 
-const API_URL = "https://cuddly-telegram-7vwxq565q7vq2944-3001.app.github.dev/api";
+const API_URL = import.meta.env.VITE_BACKEND_URL || "/api"
 
 export const CreatePromotorCategory = () => {
     const [promotors, setPromotors] = useState([]);
