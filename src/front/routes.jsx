d0679995f2718487f5cef36d0926e1f788ccf7b7
CarolinaNFeria <rogua20.12@gmail.com>
2026-04-27 18:12:43 +0000

    solved import

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -88,7 +88,7 @@ import { UserEvents } from "./pages/AuthUser/UserEvents";
 import { UserProfiles } from "./pages/AuthUser/UserProfiles";
 import { UserGroups } from "./pages/AuthUser/UserGroups";
 import { UserGroupDetail } from "./pages/AuthUser/UserGroupDetail";
-
+import { EventDetail} from "./pages/Event/EventDetail";
 export const router = createBrowserRouter(
   createRoutesFromElements(
     // CreateRoutesFromElements function allows you to build route elements declaratively.
@@ -177,6 +177,7 @@ export const router = createBrowserRouter(
       <Route path="/admin/private" element={<PrivateAdmin />} />
       <Route path="/user/saved-events" element={<MySavedEvents />} />
       <Route path="/user/assisting-events" element={<MyAssistingEvents />} />
+      <Route path="/events/:id" element={<EventDetail />} />
       <Route path="/user/register" element={<RegisterUser />} />
       <Route path="/user/events" element={<UserEvents />} />
       <Route path="/user/profiles" element={<UserProfiles />} />
