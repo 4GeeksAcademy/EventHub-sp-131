6919a6cea7021c25c72a3af82dbd4b86163f3346
Matias Otero Sarra <matiasoterosarra@gmail.com>
2026-05-06 15:38:03 +0000

    integrating emailJs

--- src/front/pages/Contact.jsx
+++ src/front/pages/Contact.jsx
@@ -1,31 +1,45 @@
-import React, { useEffect } from "react"
-import { Link } from "react-router-dom";
-import EventHubHeroImage from "../assets/img/EventHubHeroImage.png";
-import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 export const Contact = () => {
+
+    const emailJSPKey = import.meta.env.VITE_EMAIL_JS
+    const form = useRef();
+
+    function sendData(e) {
+        e.preventDefault();
+        emailjs.sendForm("service_qmfaee8", "template_yjmh02b" , form.current, {
+            publicKey: emailJSPKey
+        })
+            .then(() => {
+                console.log('SUCCESS!');
+            }, (error) => {
+                console.log('FAILED...', error);
+            });
+    }
+
     return (
         <section className="contact py-5 bg-dark" id="contact">
             <div className="container">
-                <h2 className="section-title text-center mb-5">¿Listo para crear tu próximo evento?</h2>
+                <h2 className="section-title text-center mb-5">Contacta con el equipo</h2>
                 <div className="row justify-content-center">
                     <div className="col-lg-8">
-                        <form className="contact-form">
+                        <form className="contact-form" ref={form}>
                             <div className="row">
                                 <div className="col-md-6 mb-3">
-                                    <input type="text" className="form-control" placeholder="Tu nombre" required />
+                                    <input type="text" className="form-control" placeholder="Tu nombre" required name='name'/>
                                 </div>
                                 <div className="col-md-6 mb-3">
-                                    <input type="email" className="form-control" placeholder="Correo electrónico" required />
+                                    <input type="email" className="form-control" placeholder="Correo electrónico" required name='email'/>
                                 </div>
                             </div>
                             <div className="mb-3">
-                                <input type="text" className="form-control" placeholder="Asunto" required />
+                                <input type="text" className="form-control" placeholder="Asunto" required name='title'/>
                             </div>
                             <div className="mb-4">
-                                <textarea className="form-control" rows="6" placeholder="Cuéntanos sobre tu evento..."></textarea>
+                                <textarea className="form-control" rows="6" placeholder="Cuéntanos..." name='message'></textarea>
                             </div>
-                            <button type="submit" className="btn btn-primary w-100">Enviar Mensaje</button>
+                            <button type="submit" className="btn btn-primary w-100" onClick={sendData}>Enviar Mensaje</button>
                         </form>
                     </div>
                 </div>
